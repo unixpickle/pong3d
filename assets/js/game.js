4318da1ee39a96f3c542427a46ba3a62c7f49935
Alex Nichol <unixpickle@gmail.com>
2018-12-31 10:16:00 -0500

    control paddle

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,10 @@
-WIDTH = 480;
-HEIGHT = 320;
+const WIDTH = 480;
+const HEIGHT = 320;
+
+const PADDLE_MAX_X = 0.475;
+const PADDLE_MAX_Y = 0.315;
+const PADDLE_MOUSE_X_SCALE = 0.6;
+const PADDLE_MOUSE_Y_SCALE = 0.4;
 
 class Game {
   constructor() {
@@ -12,7 +17,10 @@ class Game {
     this.renderer.domElement.id = 'game-container';
     document.body.appendChild(this.renderer.domElement);
 
-    this.step(0);
+    this._lastTime = performance.now();
+    window.requestAnimationFrame((t) => this._animationFrame(t));
+
+    this._setupMouseEvents();
   }
 
   step(t) {
@@ -23,6 +31,23 @@ class Game {
     scene.add(this.enemyPaddle.object());
     scene.add(this.playerPaddle.object());
     this.renderer.render(scene, this.camera);
+  }
+
+  _animationFrame(t) {
+    this.step(t - this._lastTime);
+    this._lastTime = t;
+    window.requestAnimationFrame((t) => this._animationFrame(t));
+  }
+
+  _setupMouseEvents() {
+    window.addEventListener('mousemove', (e) => {
+      const el = this.renderer.domElement;
+      const box = el.getBoundingClientRect();
+      const x = PADDLE_MOUSE_X_SCALE * (2 * (e.clientX - box.left) / el.offsetWidth - 1);
+      const y = PADDLE_MOUSE_Y_SCALE * (1 - 2 * (e.clientY - box.top) / el.offsetHeight);
+      this.playerPaddle.x = Math.min(Math.max(-PADDLE_MAX_X, x), PADDLE_MAX_X);
+      this.playerPaddle.y = Math.min(Math.max(-PADDLE_MAX_Y, y), PADDLE_MAX_Y);
+    });
   }
 }
 
